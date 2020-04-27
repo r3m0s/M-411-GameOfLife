c071707cf5b841a98fd76c51dc59b8ce91f9329f
Remo Schaller <schallerremo@gmail.com>
2020-04-27 16:52:39 +0200

    Game-Struktur programmiert

--- source/src/main/java/GameOfLife.java
+++ source/src/main/java/GameOfLife.java
@@ -1,16 +1,71 @@
+import java.util.Random;
+
+import static java.lang.String.valueOf;
+import static java.lang.Thread.sleep;
+
 public class GameOfLife {
-    private int width, height;
+    private int vertical, horizontal;
+    private int [] [] [] generation;
 
-    public GameOfLife(int width, int height) {
-        this.setSize(width, height);
+    GameOfLife(int vertical, int horizontal, int generations) {
+        this.setSize(vertical, horizontal, generations);
+        this.setupGame();
     }
 
-    void startGame() {
-
+    private void setupGame() {
+        Random random = new Random();
+        int t = 0;
+        for(int x = 0; x < vertical; x++) {
+            for(int y = 0; y < horizontal; y++) {
+                generation[x][y][t] = random.nextInt(2);
+            }
+        }
     }
 
-    void setSize(int width, int height) {
-        this.width = width;
-        this.height = height;
+    void runGame(int generation) throws InterruptedException {
+        showGame(generation);
+        validateRules(generation);
+        sleep(10000);
+    }
+
+    private void validateRules(int t) {
+        for (int x = 0; x < vertical; x++) {
+            for (int y = 0; y < horizontal; y++) {
+                int neighbors = 0;
+                for (int h = x - 1; h <= x + 1; h++) {
+                    if (!inboundsVertical(h)) { continue; }
+                    for (int v = y - 1; v <= y + 1; v++) {
+                        if (!inboundsHorizontal(v)) { continue; }
+                        if(generation[h][v][t] == 1) { neighbors++; }
+                    }
+                }
+                neighbors -= generation[x][y][t];
+                generation[x][y][t+1] = (neighbors < 2 || neighbors > 3) ? 0 : (neighbors == 3) ? 1 : generation[x][y][t];
+            }
+        }
+    }
+
+    public void showGame(int t) {
+        for (int x = 0; x < vertical; x++) {
+            for (int y = 0; y < horizontal; y++) {
+                int element = generation[x][y][t];
+                System.out.print(element + "  ");
+            }
+            System.out.println();
+        }
+    }
+
+    private void setSize(int vertical, int horizontal, int generations) {
+        this.vertical = vertical;
+        this.horizontal = horizontal;
+        generation = new int [vertical] [horizontal] [generations];
+    }
+
+    private boolean inboundsVertical(int index){
+        return (index >= 0) && (index < vertical);
+    }
+
+    private boolean inboundsHorizontal(int index){
+        return (index >= 0) && (index < horizontal);
     }
 }
--- source/src/main/java/main.java
+++ source/src/main/java/main.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class main {
-    public static void main(String[] args) {
-        GameOfLife gameOfLife = new GameOfLife(10, 10);
-        gameOfLife.startGame();
+    public static void main(String[] args) throws InterruptedException {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Vertical: ");
+        int v = in.nextInt();
+        System.out.println("Horizontal: ");
+        int h = in.nextInt();
+        System.out.println("Generations to calculate: ");
+        int g = in.nextInt();
+
+        GameOfLife gameOfLife = new GameOfLife(v, h, g);
+        for (int y = 0; y < g-1; y++) {
+            System.out.println();
+            System.out.println("GENERATION " + y);
+            gameOfLife.runGame(y);
+        }
+        System.out.println();
+        System.out.println("GENERATION " + (g-1));
+        gameOfLife.showGame(g-1);
     }
 }
