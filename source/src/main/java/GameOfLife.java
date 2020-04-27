c6dd7860a142ffe0da821b671b28decd8abc5f53
Remo Schaller <schallerremo@gmail.com>
2020-04-27 14:40:27 +0200

    Start-Klasse hinzugefügt

---
+++ source/src/main/java/GameOfLife.java
@@ -0,0 +1,16 @@
+public class GameOfLife {
+    private int width, height;
+
+    public GameOfLife(int width, int height) {
+        this.setSize(width, height);
+    }
+
+    void startGame() {
+
+    }
+
+    void setSize(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+}
